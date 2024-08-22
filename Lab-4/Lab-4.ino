9f858b9ae6622b81edd7f3c3ceaaea92a09f4960
AliceuJunior <127908030+AliceuJunior@users.noreply.github.com>
2024-08-21 21:22:42 -0300

    Add files via upload

---
+++ Lab-4/Lab-4.ino
@@ -0,0 +1,27 @@
+// C++ code
+//
+int led1=11;
+int led2=10;
+int led3=9;
+void setup()
+{
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(led1, HIGH);
+  delay(1000);
+  digitalWrite(led1, LOW);
+  delay(1000);
+  digitalWrite(led2, HIGH);
+  delay(1000);
+  digitalWrite(led2, LOW);
+  delay(1000);
+  digitalWrite(led3, HIGH);
+  delay(1000);
+  digitalWrite(led3, LOW);
+  delay(1000);
+}
